63ec6f3b9ee1a2c3b1f43281b268e7626db94e92
Andrew Markham-Davies <atmd@andrews-mbp.home>
2023-09-19 18:46:31 +0100

    fix: use square image for mobile

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,22 +1,36 @@
 import Image from 'next/image';
-import { isMobile } from 'react-device-detect';
 
 const Hero = () => {
 
     return (
-        <Image
-            className={'rounded-t-lg'}
-            sizes="100vw"
-            width={300}
-            height={300}
-            // className="h-auto max-w-full"
-            src={isMobile ? "/images/poster-mobile.png" : "/images/poster.png" }
-            alt="image description"
-            style={{
-                width: '100%',
-                height: 'auto',
-            }}
-        />
+        <>
+            <Image
+                className={'hidden md:block rounded-t-lg'}
+                sizes="100vw"
+                width={300}
+                height={300}
+                // className="h-auto max-w-full"
+                src={"/images/poster.png" }
+                alt="image description"
+                style={{
+                    width: '100%',
+                    height: 'auto',
+                }}
+            />
+            <Image
+                className={'block md:hidden rounded-t-lg'}
+                sizes="100vw"
+                width={300}
+                height={300}
+                // className="h-auto max-w-full"
+                src={"/images/poster-mobile.png" }
+                alt="image description"
+                style={{
+                    width: '100%',
+                    height: 'auto',
+                }}
+            />
+        </>
     );
 }
 
