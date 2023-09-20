3d90dda40a8b68bded3e278ffe473c5914f3b896
Andrew Markham-Davies <atmd@Andrews-MacBook-Pro.local>
2023-09-20 12:21:16 +0100

    chore: remove talent name

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,10 +5,10 @@ const Header = () => {
                 <a className="flex items-center mb-0 font-medium text-gray-900 title-font">
                     <img width={80} src={'/images/smart-box-office-logo-002@2x.png'} alt={''} />
                 </a>
-                <nav className="flex items-center justify-center text-base md:ml-auto">
-                    <span className="mr-5 hover:text-gray-900">Talent_ &nbsp; Amir Khan</span>
-                    {/*<a href="#_" className="hover:text-gray-900">Contact</a>*/}
-                </nav>
+                {/*<nav className="flex items-center justify-center text-base md:ml-auto">*/}
+                {/*    <span className="mr-5 hover:text-gray-900">Talent_ &nbsp; Amir Khan</span>*/}
+                {/*    /!*<a href="#_" className="hover:text-gray-900">Contact</a>*!/*/}
+                {/*</nav>*/}
             </div>
         </header>
     );
