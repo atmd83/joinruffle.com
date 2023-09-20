ada4dc26023ff9958bb7d8c8ab904279071189d9
Andrew Markham-Davies <atmd@Andrews-MacBook-Pro.local>
2023-09-20 12:20:38 +0100

    chore: update logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,7 @@ const Header = () => {
         <header className="w-full mt-4 text-white bg-black shadow-sm body-font">
             <div className="flex flex-col items-start p-4 mx-auto md:flex-row">
                 <a className="flex items-center mb-0 font-medium text-gray-900 title-font">
-                    <img width={80} src={'/images/logo.png'} alt={''} />
+                    <img width={80} src={'/images/smart-box-office-logo-002@2x.png'} alt={''} />
                 </a>
                 <nav className="flex items-center justify-center text-base md:ml-auto">
                     <span className="mr-5 hover:text-gray-900">Talent_ &nbsp; Amir Khan</span>
